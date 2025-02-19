195ad076e5b9dc5b90779f448045f8b68e28786d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2025-02-19 11:00:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rahma_interview
+ 
---
+++ migrations/20250219095449-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      prenom: {
+        type: Sequelize.STRING
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'candidat', 'recruteur')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
